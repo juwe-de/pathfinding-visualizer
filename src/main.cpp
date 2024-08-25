ee19296e993e7e2ad653a96a0a84897beedd674a
juwe-de <wendland.julius@gmail.com>
2024-08-25 20:14:53 +0200

    added glfw + opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, world!" << '\n';
+    
+    GLFWwindow* window;
+
+    if(!glfwInit()) return -1;
+
+    window = glfwCreateWindow(640, 400, "please", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Couldn't load OpenGL" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
 }
