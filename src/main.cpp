6730953445317af0e74c0930f57ab88947efee6a
juwe-de <wendland.julius@gmail.com>
2024-08-26 14:47:19 +0200

    added imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,71 @@
+#include "glad/glad.h"
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
 #include <iostream>
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+
 int main() {
-    
+
     GLFWwindow* window;
 
     if(!glfwInit()) return -1;
 
-    window = glfwCreateWindow(640, 400, "please", NULL, NULL);
+    window = glfwCreateWindow(640, 400, "Window", NULL, NULL);
     glfwMakeContextCurrent(window);
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+
     if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-        std::cout << "Couldn't load OpenGL" << std::endl;
+        std::cout << "Failed to load OpenGL." << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
-
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        ImGui::End();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
         glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
     }
 
+    ImGui_ImplGlfw_Shutdown();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
 
 }
